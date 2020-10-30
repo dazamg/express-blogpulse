07c0ab0eb0dd3a1bf18941797a3eb362d332408f
dazmag <iscope09@hotmail.com>
2020-10-30 16:31:21 -0400

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-require('dotenv').config()
+//require('dotenv').config()
 let express = require('express')
 let ejsLayouts = require('express-ejs-layouts')
 let db = require('./models')
 let moment = require('moment')
 let app = express()
-
 app.set('view engine', 'ejs')
-
 app.use(express.urlencoded({ extended: false }))
 app.use(ejsLayouts)
 app.use(express.static(__dirname + '/public/'))
-
 // middleware that allows us to access the 'moment' library in every EJS view
 app.use((req, res, next) => {
   res.locals.moment = moment
   next()
 })
-
 // bring in authors and articles controllers
 app.use('/authors', require('./controllers/authors'))
 app.use('/articles', require('./controllers/articles'))
-
 // GET / - display all articles and their authors
 app.get('/', (req, res) => {
   db.article.findAll({
@@ -32,7 +27,6 @@ app.get('/', (req, res) => {
     res.status(400).render('main/404')
   })
 })
-
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log(`You're listening to the smooth sounds of port ${process.env.PORT}`)
 })
